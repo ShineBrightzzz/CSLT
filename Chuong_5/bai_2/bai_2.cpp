1cefd9bb24fb3bf71ab5dd92f434e479a9f20b52
Ninh Hai Nam <ninhhainam12@gmail.com>
2024-05-04 14:53:00 +0700

    push bai_2

--- Chuong_5/bai_2/bai_2.cpp
+++ Chuong_5/bai_2/bai_2.cpp
@@ -15,48 +15,99 @@ void nhapMang(int a[],int &n){
 }
 
 void checkOddEven(int a[],int n){
-	bool odd = true;
-	bool even = true;
+	bool isOdd = true;
+	bool isEven = true;
 	for(int i=0;i<n;i++){
 		if(a[i]%2!=0){
-			even = false;
+			isEven = false;
 			break;
 		}
 	}
 
 	for(int i=0;i<n;i++){
 		if(a[i]%2==0){
-			odd = false;
+			isOdd = false;
 			break;
 		}
 	}
 
-	if(odd == true){
-		cout<<"Odd";
+	if(isOdd == true){
+		cout<<"Odd"<<endl;
 	}
-	else if(even ==true){
-		cout<<"Even";
+	else if(isEven ==true){
+		cout<<"Even"<<endl;
 	}else{
-		cout<<"Not odd either even";
+		cout<<"Neither Odd nor Even"<<endl;
 	}
 	
 }
+	
+ void checkPrime(int a[],int n){
+ 	bool isPrime = true;
+ 	for(int i=0;i<n;i++){
+ 		for(int j=2;j<a[i];j++){
+ 			if(a[i]%j==0){
+ 				isPrime = false;
+ 				break;
+ 			}
+ 		}
+ 	}
 
-// void checkPrime(){
+ 	if(isPrime){
+ 		cout<<"Prime"<<endl;
+ 	}else{
+ 		cout<<"notPrime"<<endl;
+ 	}
+ }
 
-// }
+ void checkSquare(int a[],int n){
+ 	bool isSquare = false;
+ 	int SquareCount =0;
+ 	for(int i=0;i<n;i++){
+ 		isSquare = false;
+ 		for(int j=1;j<=a[i];j++){
+ 			if(a[i]==j*j){
+ 				isSquare = true;
+ 				SquareCount++;
 
-// void checkSquare(){
+ 			}
+ 		}
+ 	}
 
-// }
+ 	if(SquareCount == n){
+ 		cout<<"Square"<<endl;
+ 	}else{
+ 		cout<<"notSquare"<<endl;
+ 	}
+}
 
-// void checkAsc(){
+void checkAsc(int a[],int n){
+	bool isAsc = true;
+	for(int i=0;i<n-1;i++){
+		if(a[i]>a[i+1]){
+			isAsc = false;
+			break;
+		}
+	}
 
-// }
+	if(isAsc){
+		cout<<"Day tang";
+	}
+}
 
-// void checkDesc(){
+void checkDesc(int a[],int n){
+	bool isDesc = true;
+	for(int i=0;i<n-1;i++){
+		if(a[i]<a[i+1]){
+			isDesc = false;
+			break;
+		}
+	}
 
-// }
+	if(isDesc){
+		cout<<"Day giam";
+	}
+}
 
 
 int main(){
@@ -64,4 +115,8 @@ int main(){
 	int a[MAX];
 	nhapMang(a,n);
 	checkOddEven(a,n);
+	checkPrime(a,n);
+	checkSquare(a,n);
+	checkAsc(a,n);
+	checkDesc(a,n);
 }
